c98887825e5c9b5ef00af125d99a1933a4768628
Wanderson Pedrosa <p.silva0399@gmail.com>
2021-08-30 09:16:20 -0300

    Compactei as imagens e tornei o site mais responsivo

--- script/inicio.js
+++ script/inicio.js
@@ -2,6 +2,7 @@ var altera = {
    cabecalho: document.querySelector('.convite'),
    optarMusica: document.querySelector('#optar-musica'),
 
+   nomeMusica: document.querySelector('.nome-musica'),
    player: document.querySelector('#player'),
    jornada: document.querySelector('#jornada')
 }
@@ -22,7 +23,9 @@ clicaNoBotao.inicial.addEventListener('click', () => {
 clicaNoBotao.aceitaSom.addEventListener('click', () => {
    altera.optarMusica.classList.toggle('active')
 
+   altera.nomeMusica.classList.toggle('active')
    altera.player.classList.toggle('active')
+
    altera.jornada.classList.toggle('active')   
 })
 
