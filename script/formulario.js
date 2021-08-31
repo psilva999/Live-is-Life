cdee16d728bdc7a0d9907e85e9eb8e9443259dd0
Wanderson Pedrosa <p.silva0399@gmail.com>
2021-08-31 12:49:12 -0300

    Solução para a lentidão do site

--- script/formulario.js
+++ script/formulario.js
@@ -2,6 +2,9 @@ var botaoQue = {
    entregaPrimeiraEtapa: document.querySelector('#infos-primeira-etapa'),
    entregaSegundaEtapa: document.querySelector('#infos-segunda-etapa'),
 
+   voltaPrimeiraEtapa: document.querySelector('#volta-primeira-etapa'),
+   voltaSegundaEtapa: document.querySelector('#volta-segunda-etapa'),
+
    finalizaEtapas: document.querySelector('#entregar-formulario')
 }
 
@@ -9,7 +12,8 @@ var mudaPara = {
    primeiraParteForm: document.querySelector('.primeira-parte'),
    segundaParteForm: document.querySelector('.segunda-parte'),
 
-   terceiraParteForm: document.querySelector('.terceira-parte')
+   terceiraParteForm: document.querySelector('.terceira-parte'),
+   resultado: document.querySelector('.essencia')
 }
 
 botaoQue.entregaPrimeiraEtapa.addEventListener('click', () => {
@@ -26,4 +30,18 @@ botaoQue.entregaSegundaEtapa.addEventListener('click', () => {
 
 botaoQue.finalizaEtapas.addEventListener('click', () => {
    mudaPara.terceiraParteForm.classList.toggle('active')
+
+   mudaPara.resultado.classList.add('active')
+})
+
+botaoQue.voltaPrimeiraEtapa.addEventListener('click', () => {
+   mudaPara.segundaParteForm.classList.toggle('active')
+
+   mudaPara.primeiraParteForm.classList.add('active')
+})
+
+botaoQue.voltaSegundaEtapa.addEventListener('click', () => {
+   mudaPara.terceiraParteForm.classList.toggle('active')
+
+   mudaPara.segundaParteForm.classList.add('active')
 })
