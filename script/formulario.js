89470ccd93ec2fb295573fad5b8810b8dcdadc5c
Wanderson Pedrosa <p.silva0399@gmail.com>
2021-09-03 09:08:45 -0300

    Compactei as imagens e gifs para carregar mais rápido para o usuario

--- script/formulario.js
+++ script/formulario.js
@@ -14,24 +14,22 @@ saberSeTreinaFisico.addEventListener('click', () => {
    var mudaGif = document.querySelector('#gif-exercicios')
 
    if (exercicios.gostaDemais.checked) {
-      mudaGif.src = '/midia/gifs/gosto-muito.gif'
+      mudaGif.src = 'https://i.ibb.co/VYvJ0wz/gosto-muito.gif'
    }
 
    else if (exercicios.fazConstantemente.checked) {
-      mudaGif.src = '/midia/gifs/constante.gif'
+      mudaGif.src = 'https://i.ibb.co/W2Yd8W1/constante.gif'
    }
 
    else if (exercicios.maisMenos.checked) {
-      mudaGif.src = '/midia/images/mais-menos.webp'
+      mudaGif.src = 'https://i.ibb.co/v1Rj7Nk/mais-menos.webp'
    }
 
    else if (exercicios.bemComFisico.checked) {
-      mudaGif.src = '/midia/gifs/ryan-pizza.gif'
+      mudaGif.src = 'https://i.ibb.co/0qfK8kk/ryan-pizza.gif'
    }
 
    else if (exercicios.sedentario.checked) {
-      mudaGif.src = '/midia/images/sedentario.jpg'
+      mudaGif.src = 'https://i.ibb.co/vXFCr3C/sedentario.jpg'
    }
 })
-
-//exercicios.mudaGif.src = 'midia/video-audios-gif/'
