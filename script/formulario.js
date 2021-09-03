9a8437e8b87a3d97deddc68066d7648b1c5ae70a
Wanderson Pedrosa <p.silva0399@gmail.com>
2021-09-03 14:12:49 -0300

    Adicionei novas pastas e alterei as imagens/gifs

--- script/formulario.js
+++ script/formulario.js
@@ -14,22 +14,22 @@ saberSeTreinaFisico.addEventListener('click', () => {
    var mudaGif = document.querySelector('#gif-exercicios')
 
    if (exercicios.gostaDemais.checked) {
-      mudaGif.src = 'https://i.ibb.co/VYvJ0wz/gosto-muito.gif'
+      mudaGif.src = '../midia/gifs/gosto-muito.gif'
    }
 
    else if (exercicios.fazConstantemente.checked) {
-      mudaGif.src = 'https://i.ibb.co/W2Yd8W1/constante.gif'
+      mudaGif.src = '../midia/gifs/constante.gif'
    }
 
    else if (exercicios.maisMenos.checked) {
-      mudaGif.src = 'https://i.ibb.co/v1Rj7Nk/mais-menos.webp'
+      mudaGif.src = '../midia/images/mais-menos.webp'
    }
 
    else if (exercicios.bemComFisico.checked) {
-      mudaGif.src = 'https://i.ibb.co/0qfK8kk/ryan-pizza.gif'
+      mudaGif.src = '../midia/gifs/ryan-pizza.gif'
    }
 
    else if (exercicios.sedentario.checked) {
-      mudaGif.src = 'https://i.ibb.co/vXFCr3C/sedentario.jpg'
+      mudaGif.src = '../midia/images/sedentario.jpg'
    }
 })
