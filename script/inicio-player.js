e3547027ae73ebfdce039bab1d8f24c4e03b537d
Wanderson Pedrosa <p.silva0399@gmail.com>
2021-09-05 08:52:47 -0300

    Adicionei um botão de restart

--- script/inicio-player.js
+++ script/inicio-player.js
@@ -22,33 +22,63 @@ var mexeNo = {
 
    play: document.querySelector('.play'),
    pause: document.querySelector('.pause'),
+   restart:document.querySelector('.restart'),
 
    voltaQuinzeSeg: document.querySelector('.volta-15-segundos'),
    avancaQuinzeSeg: document.querySelector('.avanca-15-segundos'),
 
    voltaMusica: document.querySelector('.skip-back'),
-   trocaMusica: document.querySelector('.skip-forward')
+   trocaMusica: document.querySelector('.skip-forward'),
+   desabilitaTrocaMusica: document.querySelector('.skip-forward-off'),
+
+   ligaDesligaLegenda: document.querySelector('.subtitle')
 }
 
 clicaNoBotao.inicial.addEventListener('click', () => {
    altera.cabecalho.classList.remove('active')
-
    altera.optarMusica.classList.toggle('active')
 })
 
 clicaNoBotao.recusaSom.addEventListener('click', () => {
    altera.optarMusica.classList.remove('active')
-
    altera.jornada.classList.add('active')
 })
 
+function retiraActivePlayPause() {
+   mexeNo.play.classList.remove('active')
+
+   mexeNo.pause.classList.remove('active')
+}
+
+function pausePlay() {
+   if (mexeNo.pause.classList.contains('active')) {
+      mexeNo.audio.play()
+   }
+
+   else {
+      mexeNo.audio.pause()
+   }
+}
+
 function tempoVideo() {
    var tempoDoAudioEm = {
-      minutos: parseInt(mexeNo.audio.duration / 60, 10),
-      segundos: ((mexeNo.audio.duration % 60) - 1).toFixed(0)
+      minutos:parseInt(mexeNo.audio.duration / 60, 10),
+      segundos:((mexeNo.audio.duration % 60) - 1).toFixed(0)
    }
 
    mexeNo.tempoTotalDaMusica.innerHTML = `0${tempoDoAudioEm.minutos}:${tempoDoAudioEm.segundos}`
+}
+
+function proximoSom() {
+   mexeNo.trocaMusica.style.display = 'none'
+   mexeNo.desabilitaTrocaMusica.style.display = 'block'
+
+   mexeNo.audio.src = 'midia/audios/of-monsters-and-men-dirty-paws.mp3'
+   altera.nomeMusica.innerHTML = "<a href='https://www.youtube.com/watch?v=OgO2U4quFDk' target='_blank'>Of Monsters and Men – Dirty Paws<img src='midia/svg/external-link.svg'></a>"
+
+   mexeNo.tempoTotalDaMusica.innerHTML = `04:20`
+   pausePlay()
+   noFinal()
 }
 
 function mudaMinutosSegundos() {
@@ -65,11 +95,53 @@ function mudaMinutosSegundos() {
    else {
       aparece.momentoDaMusica.innerHTML = `0${aparece.mudaMinutos}:${aparece.mudaSegundos}`
    }
+
+   if (aparece.mudaMinutos == 4 && aparece.mudaSegundos == 25) {
+      proximoSom()
+   }  
+
+   retiraPlay()
+   function retiraPlay() {
+      if (aparece.mudaMinutos == 4 && aparece.mudaSegundos >= 20 && mexeNo.restart.classList.contains('active')) {
+         retiraActivePlayPause()
+      }
+   }
+
+   noFinal()
 }
+
+function noFinal() {
+   mexeNo.audio.onended = function restartFinalMusica() {
+      retiraActivePlayPause()
+      mexeNo.restart.classList.add('active')
+
+      if (mexeNo.audio.currentTime == 0) {
+         mexeNo.pause.classList.add('active')
+
+         mexeNo.restart.classList.remove('active')
+      }
+   }
+}
+
+function pulaQuinzePraTras() {
+   mexeNo.audio.currentTime += Number(this.dataset.skip)
+
+   if (mexeNo.restart.classList.contains('active')) {
+      mexeNo.restart.classList.remove('active')
+
+      mexeNo.pause.classList.add('active')
+      mexeNo.audio.play()
+   }
+}
+mexeNo.voltaQuinzeSeg.addEventListener('click', pulaQuinzePraTras)
+
+function pulaQuinzePraFrente() { mexeNo.audio.currentTime += Number(this.dataset.skip) }
+mexeNo.avancaQuinzeSeg.addEventListener('click', pulaQuinzePraFrente, noFinal)
 
 clicaNoBotao.aceitaSom.addEventListener('click', () => {
    altera.optarMusica.classList.toggle('active')
    altera.nomeMusica.classList.add('active')
+
    altera.nomeMusica.innerHTML = "<a href='https://www.youtube.com/watch?v=_HWRGKfSq3A' target='_blank'>Stay Alive - José González <img src='midia/svg/external-link.svg'></a>"
 
    altera.tempoDaMusica.classList.add('active')
@@ -77,17 +149,15 @@ clicaNoBotao.aceitaSom.addEventListener('click', () => {
 
    tempoVideo()
    mexeNo.audio.play()
-
    altera.jornada.classList.toggle('active')
 })
 
-mexeNo.audio.addEventListener('timeupdate', mudaMinutosSegundos)
-
 mexeNo.play.addEventListener('click', () => {
    mexeNo.play.classList.remove('active')
-
    mexeNo.audio.play()
    mexeNo.pause.classList.add('active')
+
+   noFinal()
 })
 
 mexeNo.pause.addEventListener('click', () => {
@@ -97,49 +167,48 @@ mexeNo.pause.addEventListener('click', () => {
    mexeNo.play.classList.add('active')
 })
 
-function skip() { mexeNo.audio.currentTime += Number(this.dataset.skip) }
-mexeNo.voltaQuinzeSeg.addEventListener('click', skip)
-mexeNo.avancaQuinzeSeg.addEventListener('click', skip)
+mexeNo.restart.addEventListener('click', () => {
+   mexeNo.restart.classList.remove('active')
+   mexeNo.pause.classList.add('active')
 
-mexeNo.trocaMusica.addEventListener('click', () => {   
-   mexeNo.voltaMusica.style.opacity = '1'
-   mexeNo.voltaMusica.style.cursor = 'pointer'
-   
-   mexeNo.audio.src = 'midia/audios/of-monsters-and-men-dirty-paws.mp3'
-   altera.nomeMusica.innerHTML = "<a href='https://www.youtube.com/watch?v=OgO2U4quFDk' target='_blank'>Of Monsters and Men – Dirty Paws<img src='midia/svg/external-link.svg'></a>"
-
-   mexeNo.trocaMusica.style.cursor = 'auto'
-   mexeNo.trocaMusica.style.opacity = '.5'
-
-   if (mexeNo.pause.classList.contains('active')) {
-      mexeNo.audio.play()
-   }
-
-   else {
-      mexeNo.audio.pause()
-   }
-
-   mexeNo.tempoTotalDaMusica.innerHTML = `04:20`
+   pausePlay()
+   restartFinalMusica()
 })
 
+mexeNo.audio.addEventListener('timeupdate', mudaMinutosSegundos)
+mexeNo.trocaMusica.addEventListener('click', proximoSom)
+
 mexeNo.voltaMusica.addEventListener('click', () => {
-   mexeNo.audio.src = 'midia/audios/stay-alive-jose-gonzalez.mp3'
+   if (mexeNo.audio.currentTime >= 5) {
+      mexeNo.audio.currentTime = 0
 
-   altera.nomeMusica.innerHTML = "<a href='https://www.youtube.com/watch?v=_HWRGKfSq3A' target='_blank'>Stay Alive - José González <img src='midia/svg/external-link.svg'></a>"
+      if (mexeNo.restart.classList.contains('active')) {
+         mexeNo.restart.classList.remove('active')
 
-   if (mexeNo.pause.classList.contains('active')) {
-      mexeNo.audio.play()
+         mexeNo.pause.classList.add('active')
+         mexeNo.audio.play()
+      }
    }
 
    else {
-      mexeNo.audio.pause()
+      mexeNo.audio.src = 'midia/audios/stay-alive-jose-gonzalez.mp3'
+      altera.nomeMusica.innerHTML = "<a href='https://www.youtube.com/watch?v=_HWRGKfSq3A' target='_blank'>Stay Alive - José González <img src='midia/svg/external-link.svg'></a>"
+
+      mexeNo.desabilitaTrocaMusica.style.display = 'none'
+      mexeNo.trocaMusica.style.display = 'block'
+      mexeNo.tempoTotalDaMusica.innerHTML = `04:24`
+
+      pausePlay()
+      restartFinalMusica()
+   }
+})
+
+mexeNo.ligaDesligaLegenda.addEventListener('click', () => {
+   if (mexeNo.ligaDesligaLegenda.classList.contains('active')) {
+      mexeNo.ligaDesligaLegenda.classList.remove('active')
    }
 
-   mexeNo.voltaMusica.style.cursor = 'auto'
-   mexeNo.voltaMusica.style.opacity = '.5'
-
-   mexeNo.trocaMusica.style.cursor = 'pointer'
-   mexeNo.trocaMusica.style.opacity = '1'
-
-   mexeNo.tempoTotalDaMusica.innerHTML = `04:25`
+   else {
+      mexeNo.ligaDesligaLegenda.classList.add('active')
+   }
 })
