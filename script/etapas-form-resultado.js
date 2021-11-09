495d342691d3c89ad33142d4a529a27e1cc55ecb
Wanderson Pedrosa <p.silva0399@gmail.com>
2021-11-08 22:25:17 -0300

    Textos e loading dos blocos ampliados

--- script/etapas-form-resultado.js
+++ script/etapas-form-resultado.js
@@ -41,6 +41,8 @@ var mudaPara = {
 }
 
 var retiraDesfocoPerguntaSensivel = document.querySelector('.suicidio')
+var longevidade_media_do_brasileiro = 76
+var essencia = longevidade_media_do_brasileiro
 
 function retiraAvisoPerguntaSensivel() {
    mudaPara.avisoPerguntaSensivel.classList.remove('active')
@@ -78,6 +80,8 @@ botaoQue.entregaPrimeiraEtapa.addEventListener('click', () => {
    var altura_user = document.querySelector('#altura-user')
    var peso_user = document.querySelector('#peso-user')
 
+   retira_essencia_da_primeira_parte()
+
    if (input_nome.value <= 0 || altura_user.value >= 2.20 || altura_user.value <= .7 || peso_user.value >= 150 || peso_user.value <= 30) {
       mensagem_erro.innerHTML = 'Insira seus dados corretamente'
       mensagem_erro.style.display = 'block'
@@ -111,17 +115,57 @@ function apagaIndicadorDeErro() {
    }
 }  
 
-botaoQue.entregaSegundaEtapa.addEventListener('click', () => {
+function retira_essencia_da_primeira_parte() {
+   var imc = {
+      peso: document.querySelector('#peso-user').value,
+      altura: document.querySelector('#altura-user').value
+   }
 
-   mudaPara.segundaParteForm.classList.toggle('active')
-   mudaPara.terceiraParteForm.classList.add('active')
-})
+   if (document.querySelector('#pessoa-sedentaria').checked && (imc.peso / (imc.altura ^ 2)) >= 25) {
+      consequencias_primeira_parte(6)
+
+      document.querySelector('#texto-do-resultado').innerHTML += "<p><a href='https://www.endocrino.org.br/obesidade-x-expectativa-de-vida/' target='_blank'>😟 - Pessoas obesas perdem de dois a dez anos de suas vidas</a></p>"
+   }
+
+   if (document.querySelector('#pessoa-sedentaria').checked && (imc.peso / (imc.altura ^ 2)) <= 18.4) {
+      consequencias_primeira_parte(5)
+
+      document.querySelector('#texto-do-resultado').innerHTML += "<p><a href='https://veja.abril.com.br/saude/estar-abaixo-do-peso-eleva-risco-de-vida-mais-do-que-obesidade/' target='_blank'>😕 - Pessoas abaixo do peso vivem cinco anos a menos</a></p>"
+   }
+
+   function consequencias_primeira_parte(modo_p_1) {
+      essencia = longevidade_media_do_brasileiro - modo_p_1
+
+      longevidade_media_do_brasileiro -= modo_p_1
+   }
+}
 
 botaoQue.voltaPrimeiraEtapa.addEventListener('click', () => {
 
    mudaPara.segundaParteForm.classList.toggle('active')
    mudaPara.primeiraParteForm.classList.add('active')
 })
+
+botaoQue.entregaSegundaEtapa.addEventListener('click', () => {
+   retira_essencia_segunda_parte()
+
+   mudaPara.segundaParteForm.classList.toggle('active')
+   mudaPara.terceiraParteForm.classList.add('active')
+})
+
+function retira_essencia_segunda_parte() {
+   if (document.querySelector('#sim-kids').checked) {
+      consequencias_segunda_parte(2)
+
+      document.querySelector('#texto-do-resultado').innerHTML += "<p><a href='https://www.dw.com/pt-br/ter-filhos-pode-aumentar-longevidade-diz-estudo/a-37936560#:~:text=Apesar de maior preocupação ao,pessoas que vivem na Suécia.' target='_blank'>👨‍👩‍👧‍👧 - Pessoas que optaram por ter filhos vivem dois anos a mais do que aquelas que optaram por não ter</a></p>"
+   }
+
+   function consequencias_segunda_parte(modo_p_2) {
+      essencia = longevidade_media_do_brasileiro + modo_p_2
+
+      longevidade_media_do_brasileiro += modo_p_2
+   }
+}
 
 botaoQue.voltaSegundaEtapa.addEventListener('click', () => {
 
@@ -140,6 +184,9 @@ botaoQue.finalizaEtapas.addEventListener('click', () => {
       document.querySelector("#loading").style.animation = 'aparece-loading-800 .3s linear forwards'
    }
 
+   retira_essencia_terceira_parte()
+   document.querySelector('#texto-do-resultado').innerHTML += "<p><a href='https://www.vittude.com/' target='_blank'>Espero que o Vittude possa te ajudar de alguma forma - 😉</a></p>"
+   
    var tempoParaOResultado = 1 * 60
    var mostraResultado = setInterval(resultado, 1000)
 
@@ -155,8 +202,47 @@ botaoQue.finalizaEtapas.addEventListener('click', () => {
          clearInterval(mostraResultado)
       }
    }
-
 })
+
+function retira_essencia_terceira_parte() {
+   if (document.querySelector('#alcool').checked) {
+      consequencias_terceira_parte(5)
+
+      document.querySelector('#texto-do-resultado').innerHTML += "<p><a href='https://www.revistaencontro.com.br/canal/atualidades/2018/04/beber-alcool-mesmo-que-de-forma-moderada-pode-fazer-mal.html' target='_blank'>🥴 - Exagerar no álcool reduz a expectativa de vida em até 5 anos</a></p>"
+   }
+
+   if (document.querySelector('#cigarro').checked) {
+      consequencias_terceira_parte(10)
+
+      document.querySelector('#texto-do-resultado').innerHTML += "<p><a href='https://www.revolucaoprateada.com.br/principais-fatores-que-influenciam-para-a-longevidade/' target='_blank'>😟 - A expectativa de vida de um fumante diminui de 20 a 25 anos</a></p>"
+   }
+
+   if (document.querySelector('#entorpecentes').checked) {
+      consequencia_das_drogas(8)
+
+      document.querySelector('#texto-do-resultado').innerHTML += "<p><a href='https://www.grupoaguiar.org/blog/drogas/quanto-tempo-vive-um-usuario-de-craque-e-como-posso-ajuda-lo/16' target='_blank'>😞 - Estima-se que um usuário de crack viva em média oito anos.</a></p>"
+   }
+
+   function consequencias_terceira_parte(modo_p_3) {
+      essencia = longevidade_media_do_brasileiro - modo_p_3
+
+      longevidade_media_do_brasileiro -= modo_p_3
+   }
+
+   function consequencia_das_drogas(drogas) {
+      const
+         data = /([0-9]{4})-([0-9]{2})-([0-9]{2})/,
+         pegarData = data.exec(document.querySelector('#nasce-user').value),
+         ano_de_nascimento = pegarData[1]
+
+      var momentoAtual = new Date().getFullYear()
+      var idade = momentoAtual - ano_de_nascimento
+
+      essencia = idade + drogas
+
+      longevidade_media_do_brasileiro = idade + drogas
+   }
+}
 
 function resultado_da_essencia() {
    const
@@ -164,14 +250,14 @@ function resultado_da_essencia() {
       pegarData = data.exec(document.querySelector('#nasce-user').value),
 
       day = pegarData[3],
-      month = pegarData[2],
+      mes = pegarData[2],
       ano_de_nascimento = pegarData[1]
 
    var momentoAtual = new Date().getFullYear()
    var mesAtual = new Date().getMonth() 
    var idade = momentoAtual - ano_de_nascimento 
 
-   var anosFinais = 76 - idade
+   var anosFinais = essencia - idade
    var anosFinaisEmDias = anosFinais * 365
 
    var essenciaEm = {
@@ -182,15 +268,33 @@ function resultado_da_essencia() {
 
    var mesesVividos = ((idade * 12) + (mesAtual - 1))
 
-   if (idade <= 15) {
+   if ((day == 30 || day == 31) && mes == 2) {
+      document.querySelector('#passa-titulo-essencia').innerHTML = `Am I a joke to you?`
+
+      space_cowboy()
+   }
+
+   else if (idade < 0) {
+      document.querySelector('#passa-titulo-essencia').innerHTML = `Stephen Hawking estaria orgulhoso de você`
+
+      space_cowboy()
+   }
+
+   else if (idade <= 15 && idade >= 0) {
+      space_cowboy()
+
       document.querySelector('#passa-titulo-essencia').innerHTML = `Muito cedo pro teste 😅`
    }
 
    else if (idade >= 77 && idade <= 100) {
+      space_cowboy()
+
       document.querySelector('#passa-titulo-essencia').innerHTML = `Aproveite sua essência 😋`
    }
 
    else if (idade >= 101) {
+      space_cowboy()
+
       document.querySelector('#passa-titulo-essencia').innerHTML = `Você não tem essa idade 😑`
    }
 
@@ -227,6 +331,16 @@ function resultado_da_essencia() {
       }
       document.querySelector(".container-dos-blocos.meses").innerHTML += ` ... + ${(essenciaEm.meses * .95).toFixed(0)}`
    }
+
+   function space_cowboy() {
+      document.querySelector('.essencia-anos').innerHTML = `See`
+      document.querySelector('.essencia-meses').innerHTML = `You`
+
+      document.querySelector('.essencia-semanas').innerHTML = `Space`
+      document.querySelector('.essencia-horas').innerHTML = `Cowboy`
+
+      document.querySelector('.container-essencia-blocos').style.display = 'none'
+   }
 }
 
 document.querySelector("#amplia-blocos-anos").onclick = () => {
@@ -261,38 +375,54 @@ document.querySelector("#amplia-blocos-anos").onclick = () => {
 }
 
 document.querySelector('#amplia-blocos-meses').onclick = () => {
-   mostra_blocos_ampliados()
+   document.querySelector('.container-dos-blocos.meses').style.filter = 'blur(5px)'
+   document.querySelector('#loading-blocos-meses').style.display = 'block'
 
-   const
-      data = /([0-9]{4})-([0-9]{2})-([0-9]{2})/,
-      pegarData = data.exec(document.querySelector('#nasce-user').value),
+   var tempoParaMostrarBlocosAmpliados = 1 * 60
+   var mostraBlocosAmpliados = setInterval(resultado, 1000)
 
-      day = pegarData[3],
-      month = pegarData[2],
-      ano_de_nascimento = pegarData[1]
+   function resultado() {
+      tempoParaMostrarBlocosAmpliados--
 
-   var momentoAtual = new Date().getFullYear()
-   var mesAtual = new Date().getMonth()
-   var idade = momentoAtual - ano_de_nascimento
+      if (tempoParaMostrarBlocosAmpliados <= 59) {
+         document.querySelector('.container-dos-blocos.meses').style.filter = 'none'
+         document.querySelector('#loading-blocos-meses').style.display = 'none'
+         mostra_blocos_ampliados()
 
-   var anosFinais = 76 - idade
-   var essenciaEm = { meses: anosFinais * 12, }
-   var mesesVividos = ((idade * 12) + (mesAtual - 1))
+         const
+            data = /([0-9]{4})-([0-9]{2})-([0-9]{2})/,
+            pegarData = data.exec(document.querySelector('#nasce-user').value),
 
-   document.querySelector("#indica-nome-bloco").innerHTML = 'Meses'
-   document.querySelector('#container-dos-blocos-ampliados').innerHTML = ""
+            day = pegarData[3],
+            month = pegarData[2],
+            ano_de_nascimento = pegarData[1]
 
-   for (var p = 0; p < ((mesesVividos * .3).toFixed(0)); p++) {
-      document.querySelector("#container-dos-blocos-ampliados").innerHTML += "<div id='meses-que-passaram'></div>"
+         var momentoAtual = new Date().getFullYear()
+         var mesAtual = new Date().getMonth()
+         var idade = momentoAtual - ano_de_nascimento
+
+         var anosFinais = 76 - idade
+         var essenciaEm = { meses: anosFinais * 12, }
+         var mesesVividos = ((idade * 12) + (mesAtual - 1))
+
+         document.querySelector("#indica-nome-bloco").innerHTML = 'Meses'
+         document.querySelector('#container-dos-blocos-ampliados').innerHTML = ""
+
+         for (var p = 0; p < ((mesesVividos * .3).toFixed(0)); p++) {
+            document.querySelector("#container-dos-blocos-ampliados").innerHTML += "<div id='meses-que-passaram'></div>"
+         }
+         document.querySelector("#container-dos-blocos-ampliados").innerHTML += ` ... + ${(mesesVividos * .7).toFixed(0)} ... `
+
+         document.querySelector("#container-dos-blocos-ampliados").innerHTML += "<div id='mes-atual'></div>"
+
+         for (var count2 = 0; count2 < ((essenciaEm.meses * .1).toFixed(0)); count2++) {
+            document.querySelector("#container-dos-blocos-ampliados").innerHTML += `<div id='meses-futuros'></div>`
+         }
+         document.querySelector("#container-dos-blocos-ampliados").innerHTML += ` ... + ${(essenciaEm.meses * .9).toFixed(0)}`
+
+         clearInterval(mostraBlocosAmpliados)
+      }
    }
-   document.querySelector("#container-dos-blocos-ampliados").innerHTML += ` ... + ${(mesesVividos * .7).toFixed(0)} ... `
-
-   document.querySelector("#container-dos-blocos-ampliados").innerHTML += "<div id='mes-atual'></div>"
-
-   for (var count2 = 0; count2 < ((essenciaEm.meses * .1).toFixed(0)); count2++) {
-      document.querySelector("#container-dos-blocos-ampliados").innerHTML += `<div id='meses-futuros'></div>`
-   }
-   document.querySelector("#container-dos-blocos-ampliados").innerHTML += ` ... + ${(essenciaEm.meses * .9).toFixed(0)}`
 }
 
 function mostra_blocos_ampliados() {
