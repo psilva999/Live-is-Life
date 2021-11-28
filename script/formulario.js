a70f447b4c130e8bc9e5800cac4ab1f1b73ea30d
Wanderson Pedrosa <p.silva0399@gmail.com>
2021-11-28 10:21:40 -0300

    Adicionei a interatividade do calendário

--- script/formulario.js
+++ script/formulario.js
@@ -1,5 +1,6 @@
 var input = {
    nome: document.querySelector('#nome-user'),
+   nascimento: document.querySelector('#nasce-user'),
    altura: document.querySelector('#altura-user'),
    peso: document.querySelector('#peso-user'),
 
@@ -16,21 +17,111 @@ var input = {
    limpo_de_substancias: document.querySelector("#limpo-substancias")
 }
 
-document.querySelector('#nasce-user').onclick = () => { teste() }
+var modifica = {
+   cobreTelaDoCalendario: document.querySelector('#tela-borrada-para-mostrar-calendario'),
+   calendario: document.querySelector('.calendario'),
 
-function teste() {
-   var nasceUser = document.querySelector('#nasce-user')
+   mes: document.querySelector('.mes'),
+   ano: document.querySelector('.ano'),
+   setaDeVoltarCalendario: document.querySelector('.volta-calendar'),
 
-   nasceUser.value = '1999-04-03'
-   nasceUser.style.background = '#F2F2F2'
-   nasceUser.style.color = 'black'
+   containerDosDias: document.querySelector('.dias'),
+   containerDosMeses: document.querySelector('#meses'),
+   containerDosAnos: document.querySelector('#anos'),
 
-   nasceUser.style.animation = 'none'
-   nasceUser.style.border = '.1rem solid transparent'
+   botaoDoCalendario: document.querySelector('#resultado-calendario')
+}
 
-   nasceUser.style.backgroundClip = 'none'
-   nasceUser.style.opacity = '.8'
-   nasceUser.style.fontFamily = "'Baloo Thambi 2', cursive"
+input.nascimento.onclick = () => {
+   modifica.cobreTelaDoCalendario.style.display = 'block'
+   modifica.calendario.style.display = 'block'
+
+   modifica.cobreTelaDoCalendario.style.animation = 'mostra-calendario .1s linear forwards'
+   modifica.calendario.style.animation = 'mostra-calendario .2s linear forwards'
+   // input.nascimento.value = `1999-04-03`
+   // input.nascimento.style.background = '#F2F2F2'
+   // input.nascimento.style.color = 'black'
+
+   // input.nascimento.style.animation = 'none'
+   // input.nascimento.style.border = '.1rem solid transparent'
+
+   // input.nascimento.style.backgroundClip = 'none'
+   // input.nascimento.style.opacity = '.8'
+   // input.nascimento.style.fontFamily = "'Baloo Thambi 2', cursive"
+}
+
+function retira_calendario() {
+   modifica.cobreTelaDoCalendario.style.display = 'none'
+   modifica.calendario.style.display = 'none'
+}
+modifica.cobreTelaDoCalendario.onclick = () => { retira_calendario() }
+
+document.querySelector('#fecha-calendario').onclick = () => { retira_calendario() }
+document.querySelector("#resultado-calendario").onclick = () => { retira_calendario() }
+
+function desativa_container_dos_dias_e_button() {
+   modifica.containerDosDias.style.display = 'none'
+   modifica.botaoDoCalendario.style.display = 'none'
+}
+
+function ativa_seta_dos_dias() {
+   modifica.setaDeVoltarCalendario.classList.remove('desativado')
+   modifica.setaDeVoltarCalendario.classList.toggle('active')
+}
+
+modifica.mes.onclick = () => {
+   desativa_container_dos_dias_e_button()
+   ativa_seta_dos_dias()
+
+   modifica.containerDosAnos.style.display = 'none'
+   modifica.containerDosMeses.style.display = 'block'
+
+   modifica.mes.classList.remove('active')
+   modifica.mes.classList.add('desativado')
+   if (modifica.ano.classList.contains('desativado')) {
+
+      modifica.ano.classList.remove('desativado')
+      modifica.ano.classList.add("active")
+   }
+}
+
+modifica.ano.onclick = () => {
+   desativa_container_dos_dias_e_button()
+   ativa_seta_dos_dias()
+
+   modifica.containerDosMeses.style.display = 'none'
+   modifica.containerDosAnos.style.display = 'block'
+
+   modifica.ano.classList.remove('active')
+   modifica.ano.classList.add('desativado')
+   if (modifica.mes.classList.contains('desativado')) {
+
+      modifica.mes.classList.remove('desativado')
+      modifica.mes.classList.add("active")
+   }
+}
+
+modifica.setaDeVoltarCalendario.onclick = () => {
+   modifica.containerDosAnos.style.display = 'none'
+   modifica.containerDosMeses.style.display = 'none'
+
+   modifica.containerDosDias.style.display = 'block'
+   modifica.botaoDoCalendario.style.display = 'block'
+
+   modifica.setaDeVoltarCalendario.classList.remove('active')
+   modifica.setaDeVoltarCalendario.classList.add('desativado')
+
+   if (modifica.ano.classList.contains('desativado')) {
+
+      modifica.ano.classList.remove('desativado')
+      modifica.ano.classList.add("active")
+   }
+
+   if (modifica.mes.classList.contains('desativado')) {
+
+      modifica.mes.classList.remove('desativado')
+      modifica.mes.classList.add("active")
+   }
 }
 
 input.nome.onkeypress = function(insereNome) {
